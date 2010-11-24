57a069b9aa72f3f6f78c36ef98349ab230ce9475
Gea-Suan Lin <darkkiller@gmail.com>
2010-11-24 18:49:58 +0800

    Remove BUGS and SUPPORT.

--- lib/Lingua/Gram.pm
+++ lib/Lingua/Gram.pm
@@ -135,41 +135,6 @@ sub unigram {
 
 Gea-Suan Lin, C<< <gslin at gslin.org> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-lingua-gram at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Lingua-Gram>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Lingua::Gram
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Lingua-Gram>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Lingua-Gram>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Lingua-Gram>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Lingua-Gram/>
-
-=back
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2010 Gea-Suan Lin.
